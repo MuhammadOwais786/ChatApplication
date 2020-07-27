3c77d392b9e56c29decf723fcb15ed99c3d9d677
Muhammad owais <owaisraza2051@gmail.com>
2020-07-28 03:43:46 +0500

    CHatApplication

--- js/caht.js
+++ js/caht.js
@@ -6,7 +6,7 @@ function loginWithFB(){
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        // console.log(user)
+        console.log(user)
         alert(user)
         // ...
       }).catch(function(error) {
